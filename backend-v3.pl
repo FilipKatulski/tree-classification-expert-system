406b3e804c91719f2cc129ba4b8d4854f710385d
Natalia Brzozowska <49209159+brzozia@users.noreply.github.com>
2022-11-17 15:58:13 +0100

    server logic works

--- backend-v3.pl
+++ backend-v3.pl
@@ -1,5 +1,3 @@
-
-
 roslina(sosna,
         [kora(czarna),
         rodzaj_lisci(igly),
@@ -12,39 +10,48 @@ roslina(grab,
           owoce(grabu)
         ]).
 
-roslina(buk,
-        [cecha(kora,czarna),
-        cecha(rodzaj_lisci,liscie),
-         cecha(owoce,buku)
+roslina(brzoza,
+        [kora(biala),
+        rodzaj_lisci(liscie),
+          owoce(grabu)
         ]).
-
 
 kora(biala).
 kora(czarna).
-klasyfikator(kora, [biala, czarna]).
 
-/*sprawdza czy cecha jest w liście cech*/
+rodzaj_lisci(igly).
+rodzaj_lisci(liscie).
+
+/*sprawdza czy cecha jest w podanej liscie cech*/
 elem(Cecha, [Cecha|_]) :-  !.
 elem(Cecha, [_|Cechy]) :-  elem(Cecha, Cechy).
 
-/*sprawdza czy wszytskie cechy są w liście cech*/
+/*sprawdza czy wszytskie cechy są w podanej liscie cech*/
 all_elems([], _).
 all_elems([Cecha|Inne], Lista) :- elem(Cecha, Lista), all_elems(Inne, Lista).
 
-/*znajduje wszytskie rozliny posiadajace cechy opisane w liscie Wiedza*/
+/*znajduje wszytskie rosliny posiadajace cechy opisane w liscie Wiedza*/
 znajdz_rosliny(Wiedza, Rosliny) :- findall(Nazwa, (roslina(Nazwa, Lista), all_elems(Wiedza,Lista)), Rosliny).
 
 /*znajdz_pytanie([], Cecha, Opcje) :- roslina(_, [Cecha|_]), Cecha(Opcje).*/
 /*znajdz_pytanie([], Cecha, Opcje) :- roslina(_, [Cecha|_]).*/
 
-znajdz_pytanie([], Cecha, Opcje) :- roslina(_, [cecha(Cecha, _)|_]), klasyfikator(Cecha,Opcje).
+
+znajdz_pytanie([], Cecha, Opcje) :- roslina(_, [Fakt|_]), znajdz_opcje_faktu(Fakt, Cecha, Opcje). /*sposrod wszystkich roslin*/
 znajdz_pytanie([R|Osliny], _, _) :- length([R|Osliny], 1), !. /*koniec szukania, zostala jedna roslina*/
-znajdz_pytanie([R|Osliny], Cecha, Opcje) :- !.
+znajdz_pytanie(Rosliny, Cecha, Opcje) :- rozne_cechy(Rosliny, [Fakt|_]), znajdz_opcje_faktu(Fakt,Cecha, Opcje).
 
+/* znajduje wszystkie cechy ktorymi roznia sie dwie rosliny */
 rozne_cechy([], []).
-rozne_cechy([R|Osliny], Wynik) :- roslina(R, Lista), writeln(Lista), rozne_cechy([Osliny], SemiWynik), writeln(SemiWynik),
-    subtract(Lista, SemiWynik, Wynik), writeln(Wynik).
+rozne_cechy([R|Osliny], Wynik) :- roslina(R, Lista), rozne_cechy(Osliny, SemiWynik), subtract(Lista, SemiWynik, Wynik).
+
+/* znajduje wszytskie opcje danego faktu */
+znajdz_opcje_faktu(Cecha, Nazwa, Wynik) :-  /* Cecha == cecha rosliny np kora(biala)*/
+    Cecha=..[Nazwa,_],            /*wyciaga z tej cechy nazwe faktu np. kora */
+    Kl=..[Nazwa, Opcje],         /*tworzy nowe query zeby znalezc wszystkie argumenty tego faktu np. kora(opcje) */
+        P=..[findall, Opcje, Kl,Wynik], /*tworzy funkcje do znalezienia wszytskich opcji z uzyciem findall np. findall(Opcje, kora(Opcje), Wyniki) */
+    call(P).                   /*wykonuje funkcje*/
 
 
-
- 
+nastepny_krok(Wiedza, Rosliny, Cecha, Opcje) :-
+    znajdz_rosliny(Wiedza, Rosliny),  znajdz_pytanie(Rosliny, Cecha, Opcje).
